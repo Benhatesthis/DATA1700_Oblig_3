c226a776e2931e553fbeb8ccbeb397c89d2fccca
Bendik Enger <beeng0339@oslomet.no>
2024-04-22 23:04:33 +0200

    Fikset litt på index, appscript og ordnet orderd by etternavn.

--- src/main/resources/static/appScript.js
+++ src/main/resources/static/appScript.js
@@ -1,3 +1,4 @@
+const antallValidering = /^[1-9]\d*$/;
 const navnValidering = /^[A-Za-z]+$/;
 const telefonValidering = /^[0-9]{8}$/;
 const epostValidering = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
@@ -10,66 +11,67 @@ $("#kjop").click(function() {
     const etternavn = $("#etternavn").val();
     const telefon = $("#telefon").val();
     const epost = $("#epost").val();
-    
-    const feil = validering(film, antall, fornavn, etternavn, telefon, epost);
+    const billett = {
+        film: film,
+        antall: antall,
+        fornavn: fornavn,
+        etternavn: etternavn,
+        telefon: telefon,
+        epost: epost
+    };
 
-    if (!feil) {
-        alert("Feil ved kjøp av billett, se felter med feilmelding");
-    } else {
-        console.log("Billett:", billett);
-        $.post("/lagre", billett, function (data) {
-            // Handle response if needed
-        });
-
-        // Reset form fields
-        $("input[type='text']").val("");
-    }
-});
-
-function validering(film, antall, fornavn, etternavn, telefon, epost){
-    let feil = false;
-
-    if (antall === ""){
+    let feil = true;
+    if (antall === "" || !antallValidering.test(antall)){
         $("#antallError").html("Skriv inn ett heltall");
-        feil = true;
-    } else {
+        feil = false;
+    }
+    else {
         $("#antallError").html("");
     }
-
     if (film === ""){
         $("#filmError").html("Feil ved input av film");
-        feil = true;
-    } else {
+        feil = false;    }
+    else {
         $("#filmError").html("");
     }
-
     if (fornavn === "" || !navnValidering.test(fornavn)){
         $("#fornavnError").html("Feil ved input av fornavn");
-        feil = true;
-    } else {
+        feil = false;    }
+    else {
         $("#fornavnError").html("");
     }
-
     if (etternavn === "" || !navnValidering.test(etternavn)){
         $("#etternavnError").html("Feil ved input av etternavn");
-        feil = true;
-    } else {
+        feil = false;    }
+    else {
         $("#etternavnError").html("");
     }
-
     if (telefon === "" || !telefonValidering.test(telefon)){
         $("#telefonError").html("Feil ved input av telefon nummer");
-        feil = true;
-    } else {
+        feil = false;    }
+    else {
         $("#telefonError").html("");
     }
-
     if (epost === "" || !epostValidering.test(epost)){
         $("#epostError").html("Feil ved input av epost");
-        feil = true;
-    } else {
+        feil = false;    }
+    else {
         $("#epostError").html("");
     }
+    if(feil === false){
+        alert("Error ved kjøp av billett, se felter med feilmelding");
 
-    return feil;
-}
+    }
+    else{
+        console.log(billett)
+        $.post("/lagre", billett, function (data) {
+        });
+
+        $("#film").val("");
+        $("#antall").val("");
+        $("#fornavn").val("");
+        $("#etternavn").val("");
+        $("#telefon").val("");
+        $("#epost").val("");
+    }
+});
